4b5325b452ef2a7c9bb5b65f871e784e87ff092a
JeongUk <qjawls65@naver.com>
2022-05-01 01:43:59 +0900

    mysql과 연결 및 데이터 가져오기는 성공

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const exphbs = require("exphbs");
+const exphbs = require("express-handlebars");
 const fileupload = require("express-fileupload");
+const mysql = require("mysql");
+const Connection = require("mysql/lib/Connection");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,12 +10,51 @@ const port = process.env.PORT || 5000;
 
 app.use(fileupload());
 
+//static files
+app.use(express.static('public'))
+app.use(express.static('upload'))
+
 app.engine('hbs', require('exphbs'));
 app.set('view engine', 'hbs');
 
+//connection pool
+const pool = mysql.createPool({
+    connectionLimit :10,
+    host: '127.0.0.1',
+    user: 'root',
+    port: '3306',
+    password: '123123r',
+    database: 'userprofile',
+});
+
+pool.getConnection((err, connection) =>{
+    if(err) throw err // 연결 X
+    console.log("DB 연결 성공");
+
+});
+
+
+
 app.get('', (req, res) => {
     res.render('index');
-})
+
+
+    pool.getConnection((err, connection) =>{
+        if(err) throw err // 연결 X
+        console.log("DB 연결 성공");
+        
+        connection.query('SELECT * FROM user WHERE id ="1"', (err, rows)=>{
+            // once done, release connection
+            connection.release();
+
+            if(!err){
+                res.render('index');
+            }
+        });
+    });
+    
+
+});
 
 app.post('', (req, res) => {
 
